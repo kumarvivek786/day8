6775105dc6d84cbec57118d8dab1e206a7a81e52
vivek <ikumarvivek786@gmail.com>
2023-07-21 12:28:15 +0530

    [Vivek]Regex]

---
+++ Day8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome To Day 8 Practice Problem for Regular Expressions");
+                Console.WriteLine("Select one Option for \n 1. Write a C# program that matches a string that has an ‘a’ followed by two to three 'b'");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                    Pattern.Question1("vivekabbkumar");
+                    Console.ReadLine();
+                    break;
+
+
+                default:
+                        Console.WriteLine("Please Select Correct option");
+                        break;
+                }
+            }
+        
+    }
+}
